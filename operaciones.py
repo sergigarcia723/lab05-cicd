b476f6e00f9ad188a365c9fb3ff265b2657f4e20
sergi.garcia <sergi.garcia723@comunidadunir.net>
2024-01-22 19:23:26 +0100

    Prueba Lab05

--- operaciones.py
+++ operaciones.py
@@ -1,5 +1,20 @@
 def sumar(a, b):
     return a + b
 
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("No se pudede dividir entre 0")
+    return a / b
+
+
 if __name__ == "__main__":
     print(sumar(5,3))
+    print(restar(5,3))
+    print(multiplicar(5,3))
+    print(dividir(5,0))
--- test_operaciones.py
+++ test_operaciones.py
@@ -1,5 +1,5 @@
 import unittest
-from operaciones import sumar
+from operaciones import sumar,restar,multiplicar,dividir
 
 
 class TestSumar(unittest.TestCase):
@@ -9,6 +9,31 @@ class TestSumar(unittest.TestCase):
         self.assertEqual(sumar(4, 0), 4)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+
+class TestRestar(unittest.TestCase):
+
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(6, 0), 6)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+
+class TestMultiplicar(unittest.TestCase):
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(3, 0), 0)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+class TestDividir(unittest.TestCase):
+
+    def test_dividir(self):
+        self.assertEqual(dividir(3, 2), 1.5)
+        self.assertEqual(dividir(-1, 1), -1)
+        self.assertEqual(dividir(-1, -1), 1) 
+        with self.assertRaises(ZeroDivisionError):
+            dividir(5, 0)
 
 if __name__ == '__main__':
     unittest.main()
